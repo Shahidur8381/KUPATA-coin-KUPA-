220c7f5035e53f1921a1cf67b5d814e265bf4c9e
ATSadi <ahnafsoad@gmail.com>
2025-03-26 22:18:03 +0600

    X

--- src/providers/WagmiProvider.tsx
+++ src/providers/WagmiProvider.tsx
@@ -1,6 +1,5 @@
-import { WagmiProvider, createConfig } from 'wagmi';
+import { WagmiProvider } from 'wagmi';
 import { bsc } from 'wagmi/chains';
-import { http } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import {
